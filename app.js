4cb821161c221e08818ab53388ce0c88ae2615a5
Dinesh Kumar (C) <dinesh_budhiraja@yahoo.com>
2024-04-30 10:08:42 +0530

    experess server added

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-console.log("Hello");
-console.log("this is a node app");
-console.log(new Date());
-console.log("added end comment");
-console.log("added end comment 5");
+const app = require('express')();
+
+app.get("/api", (req,res) => {
+    res.json({ message : "api will ber served hrere"})
+})
+app.get("/", (req,res) => {
+    res.json({ message : "home for api"})
+})
+
+app.listen(8000, ()=> { console.log("server running on port 8000")})
